2d9238815a4fe9fe9d9719e6654491dc50596948
Jayesh049 <jampotom421@gmail.com>
2023-09-25 13:00:28 +0530

    completed new folder structure

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+const SignInPage = () => {
+    return ( 
+    
+    <p>This is unprotected page</p> );
+}
+ 
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+        <p>This is a protected page</p>
+        
+        </div>
+    )
+}
