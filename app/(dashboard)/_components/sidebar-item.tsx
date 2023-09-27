9e9b295e3a53b897d9107dcef9486b2844c8536c
Jayesh049 <jampotom421@gmail.com>
2023-09-27 20:06:06 +0530

    completed basic layout

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+
+interface SidebarItemProps {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+};
+
+
+export const SidebarItem = ({
+    // after defining here we can independently define these three things
+    icon: Icon,
+    label,
+    href,
+}:SidebarItemProps) =>{
+    const pathname = usePathname();
+    const router = useRouter();
+    
+    // isActive concept for pathname
+    const isActive = 
+    // for root page
+        (pathname === "/" && href === "/") ||
+    // for another href like /search {exact same page}
+        pathname === href || 
+    // for subroute of specific route / teacher->> to mulitple other routes
+        pathname?.startsWith(`${href}/`);
+
+    const onClick = () => {
+        router.push(href);
+    }
+    return (
+        <button
+        onClick={onClick}
+        type="button"
+        className={cn(
+            "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+            isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+            )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon 
+                    size={22}
+                    className={cn(
+                        "text-slate-500",
+                        isActive && "text-sky-700"
+                    )}
+                />
+                {label}
+            </div>
+            {/* border - bar */}
+            <div 
+                className={cn(
+                    "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+                    isActive && "opacity-100"
+                )}
+            />
+        </button>
+    )
+}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+
+const DashboardLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return ( 
+        <div className="h-full">
+            <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
+            {/* sidebar should be hidden  */}
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+                <Sidebar />
+            </div>
+            {/* to show the contents of children the sidebar width is 56 so we have to add ml-56 */}
+            <main
+                className="md:pl-56  h-full">
+                    {children}
+            </main>
+        </div>
+     );
+}
+ 
+export default DashboardLayout;
